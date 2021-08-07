6e6ae14775868234e0af130c1e9d73428e38198d
Adrian <adriantutuianu01@gmail.com>
2021-08-07 21:57:38 +0100

    Added challenge task

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("works");
+// 1. Create 2 persons which will contain name, surname and age. First peson name should be Caroline and second person name will be Maya- object
+// 2. Store persons in 2 separate variables as string. Format: `Name Surname` is `age` years old . And display their value. - document.write
+// 3. create a new variable called isGreater and give it the value of undefined;
+// 4. Create a function called compareAge with 2 arguments. If the first argument is greater than the second it will return true otherwise return false.
+// 5. call compareAge and pass first person age and second person age. Store the result value in isGreater variable.
+// 6.Change the age of the second person- current age + 50 years.
+// 7. Create a new variable called persons and assign an empty array.
+// 8. push person 1 and person 2 objects to array.
+// 9 . Create a new array called newPersons and assign 2 new persons(objects-  which will include name, surname, age) then create a new variable called allPersons and add as value persons and newPersons.
+// 10. Loop through allPersons and display the value of the youngest person Format : the youngest person is `Name Surname` which is `age` old.
+// 11.Create a new variable called overFifty and filter all the persons which are over 50 years old. For each person which is over fifty display it's value. Format: `Name` is over 50.
+// 12. Loop through allPersons array, find and remove the one which has name Caroline.
+// 13. Add an event listener and check when  person 2 name(Maya- from inital variable, not from allPersons array) will change it's name to any other value. When a change will be detected an alert will pop up to notify. Format: Person 2 has changed it's name. New name is `Name`.
+// 14. Change person 2 name from Maya to Charlotte.
