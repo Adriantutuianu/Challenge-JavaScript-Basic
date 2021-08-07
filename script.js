bba127eeb8d176f541681bd8054d347a5e07928e
Adrian <adriantutuianu01@gmail.com>
2021-08-07 22:43:47 +0100

    Finished - Store persons in 2 separate variables as strings

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // 1. Create 2 persons which will contain name, surname and age. First person name should be Caroline and second person name will be Maya- object
+document.write(`Task 1 <br>`);
+
 const firstPerson = {
   name: "Caroline",
   surname: "Davids",
@@ -10,18 +12,53 @@ const secondPerson = {
   surname: "James",
   age: 56,
 };
-console.log(firstPerson);
-console.log(secondPerson);
-// 2. Store persons in 2 separate variables as string. Format: `Name Surname` is `age` years old . And display their value. - document.write
+
+document.write(firstPerson.surname);
+console.log(secondPerson.name);
+
+document.write(`<br> Task 2 <br>`);
+// 2. Store persons in 2 separate variables as strings. Format: `Name Surname` is `age` years old . And display their value. - document.write
+const firstPersonDetails = `${firstPerson.name} ${firstPerson.surname} is ${firstPerson.age} years old.`;
+document.write(firstPersonDetails);
+
+// created an element in HTML and displayed the value of firstPersonDetails
+const firstElementFromHtml = document.querySelector(".first");
+firstElementFromHtml.innerHTML =
+  firstPersonDetails +
+  "- Note: the value of firstPersonDetails will be passed to the selected HTML element - class -> first";
+
 // 3. create a new variable called isGreater and give it the value of undefined;
+document.write(`<br> Task 3 <br>`);
+
 // 4. Create a function called compareAge with 2 arguments. If the first argument is greater than the second it will return true otherwise return false.
+document.write(`<br> Task 4 <br>`);
+
 // 5. call compareAge and pass first person age and second person age. Store the result value in isGreater variable.
+document.write(`<br> Task 5 <br>`);
+
 // 6.Change the age of the second person- current age + 50 years.
+document.write(`<br> Task 6 <br>`);
+
 // 7. Create a new variable called persons and assign an empty array.
+document.write(`<br> Task 7 <br>`);
+
 // 8. push person 1 and person 2 objects to array.
+document.write(`<br> Task 8 <br>`);
+
 // 9 . Create a new array called newPersons and assign 2 new persons(objects-  which will include name, surname, age) then create a new variable called allPersons and add as value persons and newPersons.
+document.write(`<br> Task 9 <br>`);
+
 // 10. Loop through allPersons and display the value of the youngest person Format : the youngest person is `Name Surname` which is `age` old.
+document.write(`<br> Task 10 <br>`);
+
 // 11.Create a new variable called overFifty and filter all the persons which are over 50 years old. For each person which is over fifty display it's value. Format: `Name` is over 50.
+document.write(`<br> Task 11 <br>`);
+
 // 12. Loop through allPersons array, find and remove the one which has name Caroline.
+document.write(`<br> Task 12 <br>`);
+
 // 13. Add an event listener and check when  person 2 name(Maya- from inital variable, not from allPersons array) will change it's name to any other value. When a change will be detected an alert will pop up to notify. Format: Person 2 has changed it's name. New name is `Name`.
+document.write(`<br> Task 13 <br>`);
+
 // 14. Change person 2 name from Maya to Charlotte.
+document.write(`<br> Task 14 <br>`);
